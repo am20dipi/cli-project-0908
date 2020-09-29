0a9708c8a7d9d2ff78d974a944f1bf08d3fb6398
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-29 18:51:33 +0000

    add desc

---
+++ lib/cli/project/0908.rb
@@ -0,0 +1,10 @@
+require "cli/project/0908/version"
+
+module Cli
+  module Project
+    module 0908
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/cli/project/0908/version.rb
@@ -0,0 +1,7 @@
+module Cli
+  module Project
+    module 0908
+      VERSION = "0.1.0"
+    end
+  end
+end
