bbe284c656b7ed4bb1069f35d5721164cbde25e0
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-30 19:45:44 +0000

    committing515

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,7 +4,7 @@ class CLI
     puts "Welcome to the Beer Punk"
     puts "_________________________"
     puts ""
-    puts "To view a delectable listing of beers please enter 'beers' or enter 'exit' to exit."
+    puts "To view a delectable listing of beers, please enter 'beers' or enter 'exit' to exit."
     puts ""
     beers = Beer.all
     input = gets.strip.downcase
@@ -19,10 +19,15 @@ class CLI
         puts "To view more info about a beer, enter the number of the beer."
         puts "Or enter 'exit' to exit."
         puts ""
+      else 
+        puts "To view a delectable listing of beers, please enter 'beers' or enter 'exit' to exit."
+        puts ""
+      end
+    end
         
-      elsif input.to_i > 0 && input.to_i <= Beer.all.length
+      if input.to_i > 0 && input.to_i <= Beer.all.length
         beer_info(input)
-        puts "To view more info about a beer, enter the number of the beer."
+        puts "To learn about a different beer, enter the number of the beer."
         puts "Or enter 'exit' to exit."
       elsif input == "exit"
         puts ""
@@ -36,17 +41,15 @@ class CLI
         puts "Or enter 'exit' to exit."
         puts ""
       end
-    end
+    
       
-  end
+  
   
   def print_beers
-    beers = Beer.all
-    beers.each.with_index(1) do |b, i|
+    Beer.all.each.with_index(1) do |b, i|
       puts "#{i}. #{b.name}"
     end
   end
   
-  
-end
+end 
     
