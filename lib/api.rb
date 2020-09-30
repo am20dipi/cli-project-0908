a11d8b0d93777f17f16784b9d2986dbe456744e5
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-30 19:35:23 +0000

    committing514

--- lib/api.rb
+++ lib/api.rb
@@ -4,21 +4,15 @@ class API
     url= "https://api.punkapi.com/v2/beers"
     uri = URI(url)
     response=Net::HTTP.get(uri)
+    beer_info =JSON.parse(response)["beers"]
     
-    beers=JSON.parse(response)["beers"].each do |b|
-      Beer.new(name: b["name"], id: b["id"]) if b["name"] != nil
+    beer_info.each do |b|
+      beers = Beer.new(name: b["name"], id: b["id"]) if b["name"] != nil
     # .parse = to turn it into something the user can consumer
     end
     #binding.pry
   end
   
-  def self.getbeer(id)
-    url = "https://api.punkapi.com/v2/beers/#{id}"
-    uri - URI(url)
-    response=Net::HTTP.get(uri)
-    
-    
-  end
 end
 
 #description: b["description"], tagline: b["tagline"], abv: b["abv"]
