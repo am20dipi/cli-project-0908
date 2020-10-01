5575f2809d8b19595403e322da52db08797830d4
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-01 15:31:55 +0000

    committing523

--- lib/cli.rb
+++ lib/cli.rb
@@ -10,39 +10,42 @@ class CLI
     beers = Beer.all
     input = gets.strip.downcase
     API.get_beers(input)
-    
-    if input == "beers"
-      puts ""
-      print_beers
-      puts "___________________"
-      puts "To view more info about a beer, enter the number of the beer."
-      puts "Or enter 'exit' to exit."
-      puts ""
-        if input.to_i > 0 && input.to_i <= Beer.all.length
-          beer_info(input)
-          puts "To view more info about a beer, enter the number of the beer."
-    elsif input == "exit"
-      puts ""
-      puts "_____________________________________________"
-      puts ""
-      puts "Thank you for using Beer Punk! Cheers! Clink!"
-      puts "_____________________________________________"
-    else
-      puts ""
-      puts "___________________"
-      puts "Sorry, I do not understand. Please enter 'beers' to view our listing." 
-      puts "Or enter 'exit' to exit."
-      puts ""
+      if input == "beers"
+        puts ""
+        print_beers
+        puts "___________________"
+        puts prompt
+        puts ""
+      elsif input == "exit"
+        puts ""
+        puts "_____________________________________________"
+        puts ""
+        puts "Thank you for using Beer Punk! Cheers! Clink!"
+        puts "_____________________________________________"
+      else
+        puts ""
+        puts "___________________"
+        puts "Sorry, I do not understand. Please enter 'beers' to view our listing." 
+        puts "Or enter 'exit' to exit."
+        puts ""
+      end
     end
       
   end
+  
+  def prompt 
+    puts "" 
+    puts "To view more info about a beer, enter the number of the beer."
+    puts "Or enter 'exit' to exit."
+    puts ""
+  end
+      
+          
   
   def print_beers
     Beer.all.each.with_index(1) do |b, i|
       puts "#{i}. #{b.name}"
     end
   end
-  
-  
-end
+
     
