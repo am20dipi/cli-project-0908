c5b5fe2c73c64311ea95f3ca5eb4aa76521fbb38
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-04 20:54:05 +0000

    comm333

--- lib/beer.rb
+++ lib/beer.rb
@@ -1,4 +1,4 @@
-class Beer #see all beers, or a see a specific beer & its corresponding information
+class Beer
   attr_accessor :name, :id, :tagline, :description, :abv, :first_brewed
   @@all = []
   
@@ -12,16 +12,11 @@ class Beer #see all beers, or a see a specific beer & its corresponding informat
     @@all << self
   end
   
-  def self.all #return of all
+  def self.all 
     @@all
   end
   
-  def self.find_by_id(id) #class constructor, returns the corresponding beer of the number input
+  def self.find_by_id(id) 
     self.all.select{|b| b.id == id}
   end
 end
-
-
-
-#attributes.each {|key, value| self.send("#{key}=", value)}
-#tagline, :description, :abv, :first_brewed,
